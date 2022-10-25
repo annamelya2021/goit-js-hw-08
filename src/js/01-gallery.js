811bae29773944306259bafea5b0e713562b2a57
annamelya2021 <anna.basyuk@gmail.com>
2022-10-25 18:10:14 +0300

    add 1 solved task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+// import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
+// Change code below this linenp
+
+console.log(galleryItems);
+
+// Add imports above this line
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ original, preview, description }) => {
+    return `
+<a class="gallery__item" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+</a>
+`;
+  })
+  .join('');
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+new SimpleLightbox('.gallery a', {
+  spinner: false, //читала що бажано не виключати цей парамент, щоб коричтувач бачив що йде загруза
+  //але мені подобається
+  captionsData: 'alt',
+  // позиція тексту по замовчуванню ботом, я змінила на топ
+  captionPosition: 'top',
+  showCounter: false,
+  captionDelay: 250,
+  navText: '←→',
+});
