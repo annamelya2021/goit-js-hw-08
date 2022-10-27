1db6fe2b4cfd32890185a9cc19d8463b7ac8fbdb
annamelya2021 <anna.basyuk@gmail.com>
2022-10-27 21:45:26 +0300

    feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  const time = data.seconds;
+  localStorage.setItem(KEY, time);
+};
+
+player.on('play', throttle(onPlay, 1000));
+
+const getStorageData = localStorage.getItem(KEY);
+if (getStorageData) {
+  player.setCurrentTime(getStorageData);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-msg';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('textarea'),
+  input: document.querySelector('.feedback-form input'),
+};
+
+let formData = {};
+onTextareaActivity();
+
+refs.form.addEventListener('submit', onFormSubmitSent);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+function onFormSubmitSent(evt) {
+  evt.preventDefault();
+  console.log(formData);
+  formData = {};
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  const savedData = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, savedData);
+}
+
+function onTextareaActivity() {
+  const saveMessage = localStorage.getItem(STORAGE_KEY);
+  if (saveMessage) {
+    const parseSave = JSON.parse(saveMessage);
+    const keys = Object.keys(parseSave);
+
+    for (const key of keys) {
+      refs.form.elements[key].value = parseSave[key];
+      formData[key] = parseSave[key];
+    }
+  }
+}
